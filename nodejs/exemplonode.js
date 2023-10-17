270f32d152769ab7ce6ef7d0189486e8c0ab9f01
PAULO BANDIERA PAIVA <paiva@unifesp.br>
2023-10-17 09:56:14 -0300

    faltou salvar alteração...

--- nodejs/exemplonode.js
+++ nodejs/exemplonode.js
@@ -4,6 +4,8 @@ const hostname = '127.0.0.1';
 const port = 3000;
  
 const servidorHTTP = http.createServer((req, res) => {
+
+
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.end(`<!DOCTYPEhtml><html><head><meta charset='UTF-8'></head><body><h2>Hello World</h2><p>URL : ${req.url} <br>Método: ${req.method} </body></html>` );
