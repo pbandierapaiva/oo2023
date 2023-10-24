cf260ee9d15f071d1b8483aad42d523faec1e6c2
PAULO BANDIERA PAIVA <paiva@unifesp.br>
2023-10-24 10:04:57 -0300

    24/10/2023 - integrando frontend e backend

---
+++ expressjs/frontend/frontend.js
@@ -0,0 +1,50 @@
+// Funções JavaScript para o FrontEnd
+
+
+function submeter() {
+	fetch('/', {
+		method: 'POST',
+		headers: {
+        		'Content-Type': 'application/json',
+      			},
+		body: JSON.stringify(
+		    	{'nome':document.getElementById("cpoNome").value}
+		    	)
+		})
+		.then( resposta => {
+			if(!resposta.ok) {
+				throw new Error('Resposta da rede n&atilde;o estava ok');
+			}
+			return( resposta.text() );
+		})
+		.then( dadoRetornado => {
+			document.getElementById('saida').innerHTML = dadoRetornado;
+		})
+		.catch(error => {
+        		alert("Erro no POST");
+      		});
+}
+
+function getSobre(){
+	fetch('/sobre')
+		.then( resposta => {
+			if(!resposta.ok) {
+				throw new Error('Resposta da rede n&atilde;o estava ok');
+			}
+			return( resposta.text() );
+		})
+		.then( dado => {
+			document.getElementById('saida').innerHTML = dado;
+		})
+		.catch( erro => {
+			document.getElementById('saida').innerHTML = erro;
+			document.getElementById('saida').style = "color:red";
+		})
+}
+
+function populaSelect() {
+	alert("popula");
+
+}
+
+
--- expressjs/servidorexpress.js
+++ expressjs/servidorexpress.js
@@ -1,28 +1,51 @@
+// Back-end ExpressJS
+
 const express = require('express')
 const arq = require('fs');
 const app = express()
 const port = 3000
 
-app.use(express.urlencoded({ 
-    extended: true
-})) 
+app.use(express.json());
+app.use(express.static('frontend'))
 
 app.get('/', (req, res) => {
-	console.log("Retornando Frontend em HTML")
-	arq.readFile('frontend.html', (err, data) =>  {
-    			res.writeHead(200, {'Content-Type': 'text/html'});
-			res.write(data);
-	    		return res.end();
-	    		})
+	res.redirect('/frontend.html');
 })
 
-app.post('/', function(req, res) {
+app.get('/sobre', (req, res) => {
+    	res.writeHead(200, {'Content-Type': 'text/html'});
+    	res.write("<h2>Exemplo de backend/frontend em ExpressJS</h2>");	
+    	return res.end();
+})
+
+app.get('/listar', (req, res) => {
+	arq.readFile('nomes.txt', 'utf8', (err, dados) => {
+		if (err) {
+		    console.error(err);
+		    return;
+		    }
+		let nomes = dados.split("\n");
+		nomes.pop();
+		res.json( nomes );
+		})
+})
+
+app.post('/', (req, res) => {
 	console.log(req.body);
+    	res.writeHead(200, {'Content-Type': 'text/html'});
+    	res.write("<h2>Dados recebidos! Nome: "+ req.body.nome + "</h2>");
     	
-    	res.redirect('/') 
+    	arq.appendFile("nomes.txt", req.body.nome+"\n", (err) => { 
+		if (err) { 
+			console.log(err); 
+		} 
+		else { 
+			console.log("\nArquivo alterado"); 
+  		} 
+	}); 	
+    	return res.end();
+
 })
-
-
 
 app.listen(port, () => {
   console.log(`Aplicação monitorando porta: ${port}`)
